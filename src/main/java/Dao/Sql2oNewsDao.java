edcd226c08971a8f65ac0a7bd031048db5c47a36
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-05-16 01:55:48 +0300

    Update NewsDao

--- src/main/java/Dao/Sql2oNewsDao.java
+++ src/main/java/Dao/Sql2oNewsDao.java
@@ -1,5 +1,51 @@
 package Dao;
 
-public class Sql2oNewsDao {
+import Interface.NewsDao;
+import module.Department;
+import module.News;
+import org.sql2o.Sql2o;
 
+import java.util.List;
+
+public class Sql2oNewsDao implements NewsDao {
+    private final Sql2o sql2o;
+
+    public Sql2oNewsDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(News news) {
+
+    }
+
+    @Override
+    public void addNewsToDepartment(News news, Department department) {
+
+    }
+
+    @Override
+    public List<News> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Department> getAllDepartmentsForANews(int id) {
+        return null;
+    }
+
+    @Override
+    public News findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
