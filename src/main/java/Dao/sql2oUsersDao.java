f1e0670baca4a1ba21bafa0f819d4f017df144ad
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-05-15 19:44:21 +0300

    Add sql dependency

---
+++ src/main/java/Dao/sql2oUsersDao.java
@@ -0,0 +1,34 @@
+package Dao;
+
+import Interface.UsersDao;
+import module.Users;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+public class sql2oUsersDao implements UsersDao {
+    private final Sql2o sql2o;
+
+    public sql2oUsersDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+
+    }
+
+
+    @Override
+    public void add(Users users) {
+
+        String sql = "INSERT INTO users (name,position,role,departmentid) VALUES(:name,:position,:role,:departmentId)";
+        try (Connection conn = sql2o.open()){
+            int id = (int) conn.createQuery(sql,true)
+                    .bind(users)
+                    .executeUpdate()
+                    .getKey();
+            users.setId(id);
+
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+
+    }
+}
