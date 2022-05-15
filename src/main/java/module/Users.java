83961a9367b37dd2ed1db8e7987929f8b516033e
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-05-15 18:26:39 +0300

    create objects

--- src/main/java/module/Users.java
+++ src/main/java/module/Users.java
@@ -7,4 +7,11 @@ public class Users {
     private int departmentId;
     private int id;
 
+    public Users(String name, String position, String role, int departmentId) {
+        this.name = name;
+        this.position = position;
+        this.role = role;
+        this.departmentId = departmentId;
+    }
+
 }
